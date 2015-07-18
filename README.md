3beb68ef836c5e5323bfc33d03e0fbaed3cae428
Carlos Santana <csantana23@gmail.com>
2015-07-18 14:28:12 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+### MobileFirst Platform CLI Helper
+- CLI helper to check if you have mfp CLI installed in your PATH and prints the version, if not installed then it prints the url to download mfp cli
+
+### Install
+
+    npm install -g mfp@latest
---
+++ index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+var spawn = require('child_process').spawn,
+    mfp  = spawn('mfp',['-v'],{stdio:'inherit'});
+mfp.on('exit', function(code) {
+    if(code !== 1){
+      console.log("Download mfp CLI from http://public.dhe.ibm.com/ibmdl/export/pub/software/products/en/MobileFirstPlatform/mobilefirst_cli_installer_7.0.0.zip");
+    } else {
+      console.log("mfp cli installed, use: `mfp help intro` for more information");
+    }
+});
