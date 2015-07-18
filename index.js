ee699468fc1acb18da919ec0eb15e8ede43df2be
Carlos Santana <csantana23@gmail.com>
2015-07-18 15:19:35 -0400

    added message for windows

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 var spawn = require('child_process').spawn,
     mfp,
     dowloadUrl = "http://public.dhe.ibm.com/ibmdl/export/pub/software/products/en/MobileFirstPlatform/mobilefirst_cli_installer_7.0.0.zip",
-    downloadMessage = "Download mfp CLI from " + dowloadUrl,
+    downloadMessage = "Download mfp CLI from: " + dowloadUrl,
     helpMessage = "mfp cli installed, use: `mfp help` for more information",
+    winMessage = "mfp detection not supported on windows, use `mfp -v` to get version or download latest mfp"
     isWin = /^win/.test(process.platform);
 if(isWin === false){
   mfp  = spawn('which',['mfp'],{stdio:['ignore', 'ignore', 'ignore']});
@@ -15,4 +16,7 @@ if(isWin === false){
         console.log(helpMessage);
       }
   });
+} else {
+  console.log(winMessage);
+  console.log(downloadMessage);
 }
