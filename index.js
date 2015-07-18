db3a89530c3664496c5d1eb5a8e6f37e722a8460
Carlos Santana <csantana23@gmail.com>
2015-07-18 14:38:03 -0400

    updated with correct return code

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 var spawn = require('child_process').spawn,
     mfp  = spawn('mfp',['-v'],{stdio:'inherit'});
 mfp.on('exit', function(code) {
-    if(code !== 1){
+    if(code !== 0){
       console.log("Download mfp CLI from http://public.dhe.ibm.com/ibmdl/export/pub/software/products/en/MobileFirstPlatform/mobilefirst_cli_installer_7.0.0.zip");
     } else {
-      console.log("mfp cli installed, use: `mfp help intro` for more information");
+      console.log("mfp cli installed, use: `mfp help` for more information");
     }
 });
